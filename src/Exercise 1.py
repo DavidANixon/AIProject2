4926ec2d15abb8e9c451ed1f116b58e4561e0f89
DavidANixon <Dman42997@gmail.com>
2018-12-04 16:39:36 -0500

    end of work session

--- src/Exercise 1.py
+++ src/Exercise 1.py
@@ -7,8 +7,10 @@ data_path = '..\irisdata.csv'
 types = [float, float, float, float, str]
 data = np.genfromtxt(data_path, delimiter=',', dtype=types,
                      names=['sepalLength', 'sepalWidth', 'petalLength', 'petalWidth', 'species'], skip_header=1)
-isNotVirginicaOrVersicolor = [data.species == 'setosa']
-relevantData = pd.DataFrame.filter(data)
 
-plt.scatter(data['petalLength'], data['petalWidth'])
+isVirginicaOrVersicolor = [data['species'] != 'setosa']
+dataframe = pd.Series(data)
+relevantData = dataframe.loc[dataframe['species' != 'setosa']]
+
+plt.scatter(relevantData['petalLength'], relevantData['petalWidth'])
 plt.show()

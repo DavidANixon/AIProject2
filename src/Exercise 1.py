49301b299b7fa8fd7cc4924ae13153ad73ddf8fb
DavidANixon <Dman42997@gmail.com>
2018-12-04 14:15:00 -0500

    loaded data

--- src/Exercise 1.py
+++ src/Exercise 1.py
@@ -1,8 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
-irisData = np.genfromtxt('..\irisdata.csv', delimiter=',', skip_header=1,
-                         names=['sepalLength', 'sepalWidth', 'petalLength', 'petalWidth', 'species'])
+data_path = '..\irisdata.csv'
 
-plt.plot(irisData['sepalLength'], irisData['species'])
+types = [float, float, float, float, str]
+data = np.genfromtxt(data_path, delimiter=',', dtype=types,
+                     names=['sepalLength', 'sepalWidth', 'petalLength', 'petalWidth', 'species'], skip_header=1)
+isNotVirginicaOrVersicolor = [data.species == 'setosa']
+relevantData = pd.DataFrame.filter(data)
+
+plt.scatter(data['petalLength'], data['petalWidth'])
 plt.show()
